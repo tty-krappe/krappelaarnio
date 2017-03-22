f9d64fb60efb6f27f621b93b95dd15520cd97833
Atte Saarinen <atte.saarinen@vincit.fi>
2017-03-22 20:00:32 +0200

    Initial command parser

--- services/modules/CommandParser.js
+++ services/modules/CommandParser.js
@@ -1,12 +1,52 @@
 'use strict'
 
+const _ = require('lodash')
+
+const ActionType = require('../enums/ActionType')
+
+const commands = [
+  {
+    actionType: ActionType.Krappe,
+    allowedInterfaces: ['irc'],
+    textCommands: ['krappe']
+  },
+  {
+    actionType: ActionType.SubstanceAbuse,
+    allowedInterfaces: ['irc', 'telegram']
+  },
+  {
+    actionType: ActionType.Wasted,
+    allowedInterfaces: ['irc', 'telegram'],
+    textCommands: ['naamat', 'naamt', 'naamta']
+  }
+]
+
 class CommandParser {
   constructor (interfaceType) {
-    this._interfaceType = interfaceType
+    this._prefix = '!'
+    this._commands = _.filter(commands, (cmd) => _.contains(cmd.allowedInterfaces, interfaceType))
   }
 
   resolve (user, message) {
-    return `${this._interfaceType}: ${user} > ${message}`
+    const parts = message.trim().split(' ')
+    const action = parts[0]
+
+    if (!action.length || action[0] !== this._prefix) {
+      return false
+    }
+
+    const cmd = _.find(this._commands, (cmd) => _.contains(cmd.textCommands, action.substr(1)))
+
+    if (!cmd) {
+      // Check if substance
+      return false
+    }
+
+    return {
+      actionType: cmd.actionType,
+      message: _.drop(parts, 1).join(' '),
+      nickname: user
+    }
   }
 }
 
