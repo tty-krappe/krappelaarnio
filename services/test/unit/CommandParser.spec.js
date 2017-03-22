9964edff9b3c382f1a2a473dd3317a7b83a9d1a1
Atte Saarinen <atte.saarinen@vincit.fi>
2017-03-22 20:00:21 +0200

    Initial command parser tests

--- services/test/unit/CommandParser.spec.js
+++ services/test/unit/CommandParser.spec.js
@@ -8,11 +8,35 @@ describe('CommandParser', () => {
     let telegramParser
 
     beforeEach(() => {
-      telegramParser = new CommandParser('tg')
+      telegramParser = new CommandParser('telegram')
     })
 
-    it('should properly resolve telegram message', () => {
-      expect(telegramParser.resolve('arttu', 'value for life')).to.equal('tg: arttu > value for life')
+    it('should properly parse !naamat command', () => {
+      const parsedCmd = telegramParser.resolve('arttu', '!naamat')
+      expect(parsedCmd.command.actionType).to.equal('wasted')
+      expect(parsedCmd.nickname).to.equal('arttu')
+      expect(parsedCmd.message).to.equal('')
+    })
+
+    it('should return false on unrecognized command', () => {
+      expect(telegramParser.resolve('laarnio', '!vittuvääräsuunta')).to.be.false
+    })
+
+    it('should properly parse !naamat aliases', () => {
+      let parsedCmd = telegramParser.resolve('kukis', '!naamt')
+      expect(parsedCmd.command.actionType).to.equal('wasted')
+
+      parsedCmd = telegramParser.resolve('masi', '!naamta')
+      expect(parsedCmd.command.actionType).to.equal('wasted')
+    })
+
+    it('should properly parse !naamat command and additional message', () => {
+      const parsedCmd = telegramParser.resolve('atte', '!naamat as fuck')
+      expect(parsedCmd.message).to.equal('as fuck')
+    })
+
+    it('should not allow !krappe command in telegram', () => {
+      expect(telegramParser.resolve('uusidiootti', '!krappe')).to.be.false
     })
   })
 
@@ -23,8 +47,26 @@ describe('CommandParser', () => {
       ircParser = new CommandParser('irc')
     })
 
-    it('should properly resolve telegram message', () => {
-      expect(ircParser.resolve('kukis', '!otanko')).to.equal('irc: kukis > !otanko')
+    it('should properly resolve !krappe command', () => {
+      const parsedCmd = ircParser.resolve('krippe', '!krappe')
+      expect(parsedCmd.command.actionType).to.equal('krappe')
+      expect(parsedCmd.nickname).to.equal('krippe')
+      expect(parsedCmd.message).to.equal('')
+    })
+
+    it('should properly resolve !krappe command and additional message', () => {
+      const parsedCmd = ircParser.resolve('krippe', '!krappe infernaalinen')
+      expect(parsedCmd.command.actionType).to.equal('krappe')
+      expect(parsedCmd.nickname).to.equal('krippe')
+      expect(parsedCmd.message).to.equal('infernaalinen')
+    })
+
+    it('should not allow commands with wrong prefix', () => {
+      expect(ircParser.resolve('foo', '.krappe')).to.be.false
+    })
+
+    it('should return false on empty message', () => {
+      expect(ircParser.resolve('foo', '')).to.be.false
     })
   })
 })
