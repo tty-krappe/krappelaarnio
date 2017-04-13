14e75ba1afe09a29d8ec355eab0723a0cdec3f88
Atte Saarinen <atte.saarinen@vincit.fi>
2017-04-13 16:16:32 +0300

    New testing config and docker image

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,34 @@
 module.exports = {
-  client: 'postgresql',
-  connection: {
-    database: 'krappe',
-    user: 'krappe',
-    password: 'laarnio',
-    port: 5434
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'krappelaarnio',
+      user: 'krappe',
+      password: 'laarnio',
+      port: 5434
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations'
+    }
   },
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    directory: './data/migrations'
+  testing: {
+    client: 'postgresql',
+    connection: {
+      database: 'krappetesti',
+      user: 'krappe',
+      password: 'laarnio',
+      port: 5434
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations'
+    }
   }
 }
