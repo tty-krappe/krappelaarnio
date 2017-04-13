ed2221151880f0e6b9f4a7536d5c7a0a99ba3756
Atte Saarinen <atte.saarinen@vincit.fi>
2017-04-13 16:17:12 +0300

    Database population script and some initial test data

---
+++ services/utils/databasePopulate.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const Promise = require('bluebird')
+const knexConfig = require('../../knexfile')
+const knex = require('knex')(knexConfig[process.env.NODE_ENV || 'development'])
+
+const db = {
+  Substance: require('../models/Substance').bindKnex(knex),
+  truncate: function () {
+    return Promise.all([
+      this.Substance.query().delete()
+    ])
+  }
+}
+
+const fromFile = (fn) => {
+  return (filePath) => fn(require(filePath))
+}
+
+const populateSubstances = (data) => {
+  db.truncate().then(() => {
+    return db.Substance.query().insertWithRelated(data)
+  })
+}
+
+module.exports = {
+  populateSubstancesFromJson: fromFile(populateSubstances),
+  populateSubstances: populateSubstances
+}
